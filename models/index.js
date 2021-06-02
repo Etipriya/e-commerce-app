cce6cc30cf25d9f778679a983c3b0f9f8b1ff972
Eti Priya <etipriya@gmail.com>
2021-06-02 21:17:22 +0100

    added product belongs to category. products, tags

--- models/index.js
+++ models/index.js
@@ -1,17 +1,32 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, { foreignKey: "category_id", onDelete: "CASCADE" });
 
 // Categories have many Products
+Category.hasMany(Product, { foreignKey: "category_id" });
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+  },
+  foreignKey: "product_id",
+  onDelete: "CASCADE",
+});
 
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+  },
+  foreignKey: "tag_id",
+  onDelete: "SET NULL",
+});
 module.exports = {
   Product,
   Category,
