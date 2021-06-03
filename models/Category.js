93d4db4c73c79b2eaeeb016be247598a914febb8
Eti Priya <etipriya@gmail.com>
2021-06-03 15:05:10 +0100

    defined columns in tag.js

--- models/Category.js
+++ models/Category.js
@@ -10,7 +10,7 @@ Category.init(
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      PrimaryKey: true,
+      primaryKey: true,
       autoIncrement: true,
     },
     category_name: {
